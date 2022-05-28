4c53ca13c66312926ff683fa4c0299e49e75fdf4
pritisharma54 <48624579+pritisharma54@users.noreply.github.com>
2022-05-28 19:39:00 +0530

    Create dsitribute_candy_interviewbit_greedy.java

---
+++ dsitribute_candy_interviewbit_greedy.java
@@ -0,0 +1,23 @@
+
+//https://www.youtube.com/watch?v=h6_lIwZYHQw
+public class Solution {
+    public int candy(ArrayList<Integer> A) {
+        int n=A.size();
+        int[] candies=new int[n];
+        Arrays.fill(candies,1);
+        for(int i=0;i<n-1;i++){
+            if(A.get(i+1)>A.get(i))
+                candies[i+1]=candies[i]+1;
+        }
+        for(int i=n-1;i>0;i--){
+            if(A.get(i-1)>A.get(i) && candies[i-1]<=candies[i])
+                candies[i-1]=candies[i]+1;
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=candies[i];
+        }
+        return sum;
+        
+    }
+}
